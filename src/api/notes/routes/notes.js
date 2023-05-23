3b8874542cc40df391853c8d9eb8220934194a64
HDDTHR <stevenjildeh@gmail.com>
2023-05-23 19:08:07 +0300

    Add create-draft policy to Notes collection

--- src/api/notes/routes/notes.js
+++ src/api/notes/routes/notes.js
@@ -6,4 +6,10 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::notes.notes');
+module.exports = createCoreRouter('api::notes.notes', {
+    config: {
+        create: {
+            policies: ['global::create-draft']
+        }
+    }
+});
