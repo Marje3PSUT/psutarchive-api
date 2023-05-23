4dadd0c988f741d6048e95d4c1367fb78fb03d0d
HDDTHR <stevenjildeh@gmail.com>
2023-05-23 19:05:25 +0300

    Create 'create-draft.js' policy for setting draft status on entry creation via endpoint

---
+++ src/policies/create-draft.js
@@ -0,0 +1,14 @@
+module.exports = (policyContext, config, { strapi }) => {
+    let obj = policyContext.request.body.data;
+
+    if (typeof (obj) == 'string') {
+        obj = JSON.parse(obj);
+    }
+
+    console.log(obj);
+
+    delete obj['publishedAt'];
+    policyContext.request.body.data = JSON.stringify(Object.assign(obj, { publishedAt: null }));
+
+    return true
+};
