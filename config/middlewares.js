1383ed271677f1d9dec87d1bb239e143e7e1c7b4
HDDTHR <stevenjildeh@gmail.com>
2023-08-22 12:46:41 +0300

    Add rateLimit middleware to global middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,22 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+
+  {
+    name: "global::rateLimit",
+
+    config: {
+      waitInterval: env("RATE_LIMIT_WAIT_INTERVAL", 2),
+      maxRequests: env("RATE_LIMIT_MAX_REQUESTS", 10),
+      endpoints: ['/api/']
+    },
+  },
 ];
