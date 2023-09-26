c6cfadb123d0da9d3ba8e934819dcbe245087c63
HDDTHR <stevenjildeh@gmail.com>
2023-09-26 16:04:40 +0300

    Add announcement collection

---
+++ src/api/announcement/routes/announcement.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * announcement router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::announcement.announcement');
